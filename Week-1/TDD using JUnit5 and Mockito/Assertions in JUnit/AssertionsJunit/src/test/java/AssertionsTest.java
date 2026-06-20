7290a6510f01de9900c1e6e90b036b0ef743e5dc
Cathy-George <cathygeorge2005@gmail.com>
2026-06-20 21:19:14 +0530

    JunitExercises

--- Week-1/TDD using JUnit5 and Mockito/Assertions in JUnit/AssertionsJunit/src/test/java/AssertionsTest.java
+++ Week-1/TDD using JUnit5 and Mockito/Assertions in JUnit/AssertionsJunit/src/test/java/AssertionsTest.java
@@ -9,15 +9,9 @@ public class AssertionsTest {
 
         // Assert equals
         assertEquals(5, 2 + 3);
-        System.out.println("assertEquals passed!");
 
         // Assert true
-        if (assertTrue(5 > 3)){
-            System.out.println("assertTrue passed!");
-        }
-        else{
-            System.out.println("assertTrue False");
-        }
+        assertTrue(5 > 3);
 
         // Assert false
         assertFalse(5 < 3);
